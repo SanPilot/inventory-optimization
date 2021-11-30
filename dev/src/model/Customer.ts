313727ead2f3c868cf11fd7bc950ab37628acecf
Hussain Khalil <hussain@sanpilot.co>
2021-11-30 13:29:06 -0500

    model: add name to customer

--- dev/src/model/Customer.ts
+++ dev/src/model/Customer.ts
@@ -7,14 +7,24 @@ import { Product } from './Product';
 export class Customer {
 
     private _preferences: Map<Product, number>;
-    private allergies: Set<Product>;
+    private _allergies: Set<Product>;
+    private _name: string;
 
-    constructor(preferences: Map<Product, number>, allergies: Set<Product>) {
+    constructor(preferences: Map<Product, number>, allergies: Set<Product>, name: string) {
         this._preferences = preferences;
-        this.allergies = allergies;
+        this._allergies = allergies;
+        this._name = name;
     }
 
     isAllergicToAny(products: Product[]) {
-        return products.some(product => this.allergies.has(product));
+        return products.some(product => this._allergies.has(product));
+    }
+
+    preferenceFor(product: Product): number {
+        return this._preferences.get(product) ?? 0;
+    }
+
+    get name(): string {
+        return this._name;
     }
 }
