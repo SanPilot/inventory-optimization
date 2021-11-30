7913212ad699a5fb04e35383d7bcaa535bb55fca
Hussain Khalil <hussain@sanpilot.co>
2021-11-30 13:19:53 -0500

    agents: add random agent

---
+++ dev/src/ai/RandomAssignmentAgent.ts
@@ -0,0 +1,43 @@
+import { Agent } from "./Agent";
+import { Order, Inventory, Assignment, Customer } from "../model";
+
+/**
+ * A search agent that randomly assigns products to customers, satisfying hard constraints and quantities.
+ */
+export class RandomAssignmentAgent implements Agent {
+
+  assign(orders: Order[], inventory: Inventory): Assignment {
+    const assignment = new Assignment();
+
+    // for each product in the inventory
+    for (const product of inventory.products()) {
+
+      // repeat for the quantity of the product
+      let remainingQuantity: number = inventory.quantityOf(product);
+      while (remainingQuantity > 0) {
+
+        // filter customers that are not allergic to the product and do not have the product already
+        const compatibleCustomers = orders.filter(([customer]) =>
+          !customer.isAllergicToAny([product]) &&
+          !assignment.productsGivenTo(customer).includes(product));
+
+        // if there are no customers that are not allergic to the product, skip this product
+        if (compatibleCustomers.length === 0) {
+          break;
+        }
+
+        // pick a random customer
+        const customer: Customer = compatibleCustomers[Math.floor(Math.random() * compatibleCustomers.length)][0];
+
+        // try assigning that product to the customer
+        try {
+          assignment.addProduct(customer, product);
+          remainingQuantity--;
+        } catch (e) {
+          console.error(e);
+        }
+      }
+    }
+    return assignment;
+  }
+}
