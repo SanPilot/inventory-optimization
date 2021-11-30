9805530f72bc902c49d8b7df774e96d9032676fa
packard-j <packard.ja@husky.neu.edu>
2021-11-30 10:28:43 -0500

    assignment cost calculation.

--- dev/src/model/Assignment.ts
+++ dev/src/model/Assignment.ts
@@ -5,6 +5,7 @@
 
 import { Product } from './Product';
 import { Customer } from './Customer';
+import _ from "lodash";
 
 export class Assignment {
   private _assignment: Map<Customer, Product[]>;
@@ -13,14 +14,27 @@ export class Assignment {
     this._assignment = new Map<Customer, Product[]>();
   }
 
-  productsOf(customer: Customer) {
+  productsGivenTo(customer: Customer): Product[] {
     return this._assignment.get(customer) ?? [];
   }
 
   addProduct(customer: Customer, product: Product) {
-    const products = this.productsOf(customer);
+    const products = this.productsGivenTo(customer);
     this._assignment.set(customer, [...products, product]);
     return this;
+  }
+
+  cost(): number {
+    return _.sum(
+      Array.from(this._assignment.keys())
+        .map(customer => this.costOfCustomer(customer))
+    )
+  }
+
+  costOfCustomer(customer: Customer) {
+    return _.sum(
+      this.productsGivenTo(customer).map(product => product.cost)
+    );
   }
 
 }
